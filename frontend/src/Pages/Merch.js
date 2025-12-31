29337c0139b1b7d939a1f0c60ccb9a961dafb0ff
Leah-saad88 <leasaad2022@icloud.com>
2025-12-31 10:52:31 +0200

    fixed the navbar to be fixed and also in the merch page i added a notification a new idea that was so nice

--- frontend/src/Pages/Merch.js
+++ frontend/src/Pages/Merch.js
@@ -3,92 +3,36 @@ import "../Assets/merch.css";
 import React, { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import "../Assets/main.css";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Merch = () => {
   const { addToCart } = useContext(CartContext);
 
   const MerchData = [
-    {
-      id: 1,
-      name: "Lando norris shirt",
-      desc: "Team's shirt available in all sizes",
-      img: Assets.l4,
-      price: 55,
-    },
-    {
-      id: 2,
-      name: "Lewis hamilton Male large",
-      desc: "Male larger/x large",
-      img: Assets.l44,
-      price: 45,
-    },
-    {
-      id: 3,
-      name: "redbull",
-      desc: "Team's shirt available in all sizes",
-      img: Assets.redbulz,
-      price: 55,
-    },
-    {
-      id: 4,
-      name: "charles leclerc merch large M/L",
-      img: Assets.lc,
-      price: 45,
-    },
-    {
-      id: 5,
-      name: "Mercedez merch",
-      desc: "Team's shirt available in all sizes",
-      img: Assets.mm2,
-      price: 51,
-    },
-    {
-      id: 6,
-      name: "Ferrari hat",
-      desc: "adjustable to any size",
-      img: Assets.Ferrar,
-      price: 49,
-    },
-    {
-      id: 7,
-      name: "Mercadez merch ",
-      desc: " White color ,Team's shirt available in all sizes",
-      img: Assets.mercc,
-      price: 61,
-    },
-    {
-      id: 8,
-      name: "aston martin merch",
-      desc: "Alonso's Team shirt available in all sizes",
-      img: Assets.astonn,
-      price: 55,
-    },
-    {
-      id: 9,
-      name: "key chain",
-      desc: "All teams available choose your likings",
-      img: Assets.key,
-      price: 21,
-    },
-    {
-      id: 11,
-      name: "Ferrari merch",
-      desc: "Team's shirt available in all sizes",
-      img: Assets.fer,
-      price: 55,
-    },
-    {
-      id: 12,
-      name: "carlos sainz ",
-      desc: "Available in Large and Medium sizes",
-      img: Assets.cs,
-      price: 55,
-    },
+    { id: 1, name: "Lando norris shirt", desc: "Team's shirt available in all sizes", img: Assets.l4, price: 55 },
+    { id: 2, name: "Lewis hamilton Male large", desc: "Male larger/x large", img: Assets.l44, price: 45 },
+    { id: 3, name: "redbull", desc: "Team's shirt available in all sizes", img: Assets.redbulz, price: 55 },
+    { id: 4, name: "charles leclerc merch large M/L", img: Assets.lc, price: 45 },
+    { id: 5, name: "Mercedez merch", desc: "Team's shirt available in all sizes", img: Assets.mm2, price: 51 },
+    { id: 6, name: "Ferrari hat", desc: "adjustable to any size", img: Assets.Ferrar, price: 49 },
+    { id: 7, name: "Mercadez merch ", desc: " White color ,Team's shirt available in all sizes", img: Assets.mercc, price: 61 },
+    { id: 8, name: "aston martin merch", desc: "Alonso's Team shirt available in all sizes", img: Assets.astonn, price: 55 },
+    { id: 9, name: "key chain", desc: "All teams available choose your likings", img: Assets.key, price: 21 },
+    { id: 11, name: "Ferrari merch", desc: "Team's shirt available in all sizes", img: Assets.fer, price: 55 },
+    { id: 12, name: "carlos sainz ", desc: "Available in Large and Medium sizes", img: Assets.cs, price: 55 },
   ];
- 
+
   const handleAdd = (product) => {
     addToCart(product);
-    alert(`${product.name} added to cart!`);
+    toast.success(`${product.name} added to cart!`, {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
   };
 
   return (
@@ -98,13 +42,15 @@ const Merch = () => {
           <img src={value.img} alt={value.name} />
           <h5>{value.name}</h5>
           <p>{value.desc}</p>
-           <p>${value.price}</p>
+          <p>${value.price}</p>
           <button onClick={() => handleAdd(value)} className="buy-btn">
             Add to Cart
           </button>
         </div>
       ))}
+      <ToastContainer />
     </div>
   );
 };
+
 export default Merch;
